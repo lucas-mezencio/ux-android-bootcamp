8d9c8b73261f0d4911a4255b1372f7497ae06ca3
Lucas Mezencio <lucasmezencio@outlook.com>
2021-08-04 14:25:05 -0300

    finish contact adapter

--- app/src/main/java/com/lucasmezencio/uxbootcampsantander/ContactAdapter.kt
+++ app/src/main/java/com/lucasmezencio/uxbootcampsantander/ContactAdapter.kt
@@ -3,6 +3,8 @@ package com.lucasmezencio.uxbootcampsantander
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>() {
@@ -22,11 +24,20 @@ class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHol
         return list.size
     }
 
-    class ContactAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun updateList(list: List<Contact>) {
+        this.list.clear()
+        this.list.addAll(list)
+        notifyDataSetChanged()  // Notifica o adapter q sua lista foi modificada
+    }
 
+    class ContactAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val tvName: TextView = itemView.findViewById(R.id.tv_name)
+        private val tvPhone: TextView = itemView.findViewById(R.id.tv_phone)
+        private val ivPhotograph: ImageView = itemView.findViewById(R.id.iv_photograph)
 
         fun bind(contact: Contact) {
-
+            tvName.text = contact.name
+            tvPhone.text = contact.phone
         }
     }
 }
