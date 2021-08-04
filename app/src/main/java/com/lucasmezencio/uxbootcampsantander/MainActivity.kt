2c5530b2f0e3656020a50276fda1b04836bbb2dd
Lucas Mezencio <lucasmezencio@outlook.com>
2021-08-04 14:56:14 -0300

    display contact list

--- app/src/main/java/com/lucasmezencio/uxbootcampsantander/MainActivity.kt
+++ app/src/main/java/com/lucasmezencio/uxbootcampsantander/MainActivity.kt
@@ -2,10 +2,42 @@ package com.lucasmezencio.uxbootcampsantander
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private val rvList: RecyclerView by lazy {
+        findViewById<RecyclerView>(R.id.rv_list)
+    }
+
+    private val adapter = ContactAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bindViews()
+        updateList()
+    }
+
+    private fun bindViews() {
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun updateList() {
+        adapter.updateList(
+            arrayListOf(
+                Contact(
+                    "Lucas Mezencio",
+                    "34 91234-5678",
+                    "img.png"
+                ),
+                Contact(
+                    "Marcio Alfredo",
+                    "11 99999-9999",
+                    "img.png"
+                )
+            )
+        )
     }
 }
